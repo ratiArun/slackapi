15d838af2b49fb64f5650b93d71b3488d1c4e833
Arun Kumar <arunkumar@Administrators-MacBook-Pro.local>
2020-04-13 20:24:55 +0530

    changed the scenarios

--- src/main/java/com/slackApi/common/BaseTest.java
+++ src/main/java/com/slackApi/common/BaseTest.java
@@ -3,6 +3,6 @@ package com.slackApi.common;
 public class BaseTest {
 	// Declaring the Variables
 	public static String BASE_URL = System.getProperty("host");
-	public static final String token = "xoxp-1064054336036-1064054336100-1078042574816-81619ba2226dcf347fd913e820b22508";
+	public static final String token = "xoxp-1064054336036-1064054336100-1080092139968-8eda40ba685e890a8bf1d292b9518eab";
 
 }
--- src/test/java/com/slackApi/tests/ChannelsTests.java
+++ src/test/java/com/slackApi/tests/ChannelsTests.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.Reporter;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -19,20 +20,107 @@ import com.slackApi.pojo.SlackApiTestsPojo;
  */
 public class ChannelsTests extends BaseTest {
 	// Declaring the variables
-	public static String channelName = Utility.getRandom9DigitNumber();
-	public static String renameChannelName = "6" + Utility.getRandom9DigitNumber();
+	public static String channelName ;
+	public static String renameChannelName ;
+	public static String channelId;
 
+	@BeforeClass
+	public void setup() {
+		channelName = Utility.getRandom9DigitNumber();
+		renameChannelName = "6" + Utility.getRandom9DigitNumber();
+	}
 	// This method will give the input to the test method
 	@DataProvider(name = "SlackApiTestsDataSuppliers")
 	public static Object[] SlackApiTestsDataSuppliers() throws Exception {
 		Object[] data = new Object[] { new SlackApiTestsPojo(channelName, true, 200) };
-
 		return data;
 	}
 
 	@Test(dataProvider = "SlackApiTestsDataSuppliers", dataProviderClass = ChannelsTests.class, priority = 0)
-	public void SlackApiTests(SlackApiTestsPojo requestData) {
+	public void createChannelTests(SlackApiTestsPojo requestData) {
 
+		// creating a channel
+		Response createChannelResponse = SlackApiMethods.createChannel(requestData.getChannelName(),
+				requestData.isValidate(), BaseTest.token);
+		Reporter.log("createChannelResponse :: " + createChannelResponse.asString());
+		// getting the channelId and channel name
+		 channelId = createChannelResponse.jsonPath().getString("channel.id");
+		String channel_Name = createChannelResponse.jsonPath().getString("channel.name");
+		Reporter.log("channelId :: " + channelId);
+
+		Assert.assertEquals(createChannelResponse.getStatusCode(), requestData.getStatusCode());
+		Assert.assertEquals(channel_Name, requestData.getChannelName(), "Channel name is mismatch");
+	}
+
+	@Test(dataProvider = "SlackApiTestsDataSuppliers", dataProviderClass = ChannelsTests.class, priority = 1)
+	public void joinChannelTests(SlackApiTestsPojo requestData) {
+
+		// joining the channel
+		Response joinChannelResponse = SlackApiMethods.joinChannel(requestData.getChannelName(),
+				requestData.isValidate(), BaseTest.token);
+		Reporter.log("joinChannelResponse :: " + joinChannelResponse.asString());
+		Assert.assertEquals(joinChannelResponse.getStatusCode(), requestData.getStatusCode());
+		Assert.assertEquals(joinChannelResponse.jsonPath().getString("channel.name"), requestData.getChannelName(),
+				"Channel name is mismatch");
+	}
+
+	@Test(dataProvider = "SlackApiTestsDataSuppliers", dataProviderClass = ChannelsTests.class, priority = 2)
+	public void renameChannelTests(SlackApiTestsPojo requestData) {
+
+		// renaming the channel name
+		Response renameChannelResponse = SlackApiMethods.renameChannel(channelId, renameChannelName, BaseTest.token);
+		String renamedChannelName = renameChannelResponse.jsonPath().getString("channel.name");
+		Reporter.log("renamedChannelName :: " + renamedChannelName);
+		Assert.assertEquals(renameChannelResponse.getStatusCode(), requestData.getStatusCode());
+		Assert.assertEquals(renamedChannelName, renameChannelName, "Channel name is mismatch");
+
+	}
+
+	@Test(dataProvider = "SlackApiTestsDataSuppliers", dataProviderClass = ChannelsTests.class, priority = 3)
+	public void validateRenameChannelTests(SlackApiTestsPojo requestData) {
+
+		// fetching all the channels
+		Response getAllChannelsResponse = SlackApiMethods.getChannels(BaseTest.token, requestData.getStatusCode());
+		Reporter.log("getAllChannelsResponse :: " + getAllChannelsResponse.asString());
+		Assert.assertEquals(getAllChannelsResponse.getStatusCode(), requestData.getStatusCode());
+		JSONArray channels = new JSONObject(getAllChannelsResponse.asString()).getJSONArray("channels");
+		for (int channel = 0; channel < channels.length(); channel++) {
+			String renamedChannelIdActual = channels.getJSONObject(channel).getString("id");
+			if (renamedChannelIdActual.equalsIgnoreCase(channelId)) {
+				Assert.assertEquals(channels.getJSONObject(channel).getString("name"), renameChannelName,
+						"Channel name is mismatch");
+			}
+		}
+	}
+
+	@Test(dataProvider = "SlackApiTestsDataSuppliers", dataProviderClass = ChannelsTests.class, priority = 4)
+	public void archiveChannelTests(SlackApiTestsPojo requestData) {
+
+		// archiving the channel
+		Response archiveChannelResponse = SlackApiMethods.archiveChannel(channelId, BaseTest.token);
+		Reporter.log("archiveChannelResponse :: " + archiveChannelResponse.asString());
+		Assert.assertEquals(archiveChannelResponse.getStatusCode(), requestData.getStatusCode());
+		Assert.assertEquals(archiveChannelResponse.jsonPath().getBoolean("ok"), true);
+	}
+
+	@Test(dataProvider = "SlackApiTestsDataSuppliers", dataProviderClass = ChannelsTests.class, priority = 5)
+	public void validateArchiveChannelTests(SlackApiTestsPojo requestData) {
+
+		// validating the channel is archived
+		Response getSpecificChannelResponse = SlackApiMethods.getSpecificChannel(BaseTest.token, channelId,
+				requestData.getStatusCode());
+		Reporter.log("getSpecificChannelResponse :: " + getSpecificChannelResponse.asString());
+		Assert.assertEquals(getSpecificChannelResponse.getStatusCode(), requestData.getStatusCode());
+		Assert.assertEquals(getSpecificChannelResponse.jsonPath().getBoolean("channel.is_archived"), true,
+				"Channel is not archived");
+	}
+
+	@Test(dataProvider = "SlackApiTestsDataSuppliers", dataProviderClass = ChannelsTests.class, priority = 6)
+	public void SlackApiEndToEndTests(SlackApiTestsPojo requestData) {
+		
+		//Creating new channel name
+		channelName = Utility.getRandom9DigitNumber();
+		renameChannelName = "6" + Utility.getRandom9DigitNumber();
 		// creating a channel
 		Response createChannelResponse = SlackApiMethods.createChannel(requestData.getChannelName(),
 				requestData.isValidate(), BaseTest.token);
